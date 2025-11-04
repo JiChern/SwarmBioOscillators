e3d7c83ee8f7d9b6f1f78ae66a40176e29088f0b
Ji Chern <135484378+JiChern@users.noreply.github.com>
2025-11-04 21:05:39 +0800

    Update fig2d.py

--- exp_codes/exp1/fig2d.py
+++ exp_codes/exp1/fig2d.py
@@ -7,7 +7,7 @@ from pathlib import Path  # Import Path for handling file paths (gcpg models are
 parent_dir = str(Path(__file__).parent.parent.parent)  # Set parent directory path for importing modules and loading files
 sys.path.append(parent_dir)  # Add parent directory to system path
 
-from agent.networks import Policy # import graph-CPG architecture
+from agent.networks import Policy # import SCPG architecture
 from environment.env import CPGEnv
 from utils import rearrange_state_vector_hopf,generate_edge_idx, state_to_goal1 
 import matplotlib.pyplot as plt
@@ -39,10 +39,10 @@ def get_stable_loss(cell_num,model,env):
     loss = 0
     
     for i in range(length):
-        # Rearrange observation to GNN inputs for graph-CPG
+        # Rearrange observation to GNN inputs for SCPG
         gnn_x = rearrange_state_vector_hopf(state=state, num_nodes=cell_num).to(device)
 
-        # Obtain external coupling terms through graph-CPG
+        # Obtain external coupling terms through SCPG
         with torch.no_grad():
             action = model(gnn_x, edge_index)
             action.clamp_(-1, 1)
@@ -75,7 +75,7 @@ def get_stable_loss(cell_num,model,env):
 
 if __name__ == '__main__':
 
-    # Set-up the graph-CPG model, with 8 attention heads
+    # Set-up the SCPG model, with 8 attention heads
     heads = 8
     fd = 64
     model = Policy(heads=heads, feature_dim=fd)
